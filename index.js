11e91ab9451145943998fb3dabdf7953d2f631cf
Ethan Toney <ethan.toney@uky.edu>
2021-10-07 18:55:28 -0400

    base version

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const port = 3000;
+const app = express();
+
+app.use(cors());
+
+app.get('/player/:username', async (req, res) => {
+	try {
+		const {data} = await axios.get('https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws', {
+			params: {
+				player: req.params.username
+			}
+		});
+		res.send(data);
+	} catch (e) {
+		res.status(e.response.status).send();
+	}
+	
+});
+
+app.get('/', (req, res) => {
+	res.sendFile('index.html', {root: __dirname});
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`)
+});
